7a65d7f9e7b921711b7d72893ce032628e58fda9
Timothy Goddard <tim@goddard.net.nz>
2013-03-19 09:55:47 +1300

    Exponential backoff on reconnect

--- plugins/ReConnect.py
+++ plugins/ReConnect.py
@@ -8,18 +8,21 @@ class ReConnectPlugin:
 		self.client = client
 		client.register_handler(self.reconnect, 
 			cflags['SOCKET_ERR'], cflags['SOCKET_HUP'], cflags['LOGIN_ERR'], cflags['AUTH_ERR'])
-		self.delay = 1
+		self.delay = 1.17
 		client.register_dispatch(self.reconnect, 0xFF)
 		client.register_dispatch(self.grab_host, 0x02)
+		client.register_dispatch(self.reset_reconnect_time, 0x03)
 
 	def reconnect(self, *args):
 		sleep(self.delay)
-		if self.delay < 128:
+		if self.delay < 300:
 			self.delay = self.delay * 2
 		self.client.login(self.host, self.port)
 
+	def reset_reconnect_time(self, *args):
+		self.delay = 1.17
+
 	#Grabs host and port on handshake
 	def grab_host(self, packet):
-		self.delay = 1
 		self.host = packet.data['host']
 		self.port = packet.data['port']
